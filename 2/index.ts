a6b097091d9ee771f5ff7fa99bfb4e29ad83d39b
Barnaby Collins <23247109+barnabycollins@users.noreply.github.com>
2025-12-04 11:51:50 +0000

    Day 2 part 2

--- 2/index.ts
+++ 2/index.ts
@@ -1,5 +1,8 @@
 import { readFile } from "fs/promises";
-import { exit } from "process";
+
+const start = performance.now();
+
+const PART: 1 | 2 = 2;
 
 type Range = [number, number];
 
@@ -9,75 +12,125 @@ function rangeInclusive(start: number, end: number) {
   return [...Array(length).keys()].map((i) => i + start);
 }
 
-function splitStringAtIndex(input: string, index: number) {
-  return input.split(new RegExp(`(?<=^.{${index}})`));
+function splitStringIntoChunks(input: string, index: number) {
+  return [...(input.match(new RegExp(`.{${index}}`, "g")) ?? [])];
 }
+
+function getFactors(v: number) {
+  if (PART === 1) return v % 2 === 0 ? [2] : [];
+
+  return Array.from(Array(Math.floor(v / 2 + 1)), (_, i) => i).filter(
+    (i) => v % i === 0,
+  );
+}
+
+const invalidIds = new Set<number>();
 
 function countInvalidIds([start, end]: Range) {
   const stringStart = start.toString();
   const stringEnd = end.toString();
 
+  console.log(`Checking range:`, start, end);
+
   const startLength = stringStart.length;
   const endLength = stringEnd.length;
 
   // possible lengths of numbers between the start and end
-  const lengthsToCheck = rangeInclusive(startLength, endLength).filter(
-    (i) => i % 2 === 0,
+  const lengthsToCheck = rangeInclusive(startLength, endLength).map(
+    (length) => ({ length, factors: getFactors(length) }),
   );
 
-  let invalidIds = 0;
+  for (const lengthEntry of lengthsToCheck) {
+    const possibleLength = lengthEntry.length;
 
-  for (const possibleLength of lengthsToCheck) {
-    const startFirstHalf = (() => {
-      if (possibleLength === startLength) {
-        const [firstHalf, secondHalf] = splitStringAtIndex(
-          stringStart,
-          possibleLength / 2,
-        ).map(Number);
+    for (const factor of lengthEntry.factors) {
+      const startFirstChunk = (() => {
+        if (possibleLength === startLength) {
+          const [firstChunk, ...remainingChunks] = splitStringIntoChunks(
+            stringStart,
+            factor,
+          ).map(Number);
 
-        // for, eg, 3638, we don't want to include 3636 - start at 3737
-        if (firstHalf < secondHalf) return firstHalf + 1;
-        else return firstHalf;
-      } else {
-        // return 10, 1000, 100000 etc
-        return parseInt(
-          `1${Array(possibleLength / 2 - 1)
-            .fill("0")
-            .join("")}`,
-        );
+          let lastChunkState: "same" | "bigger" | "smaller" = "same";
+          let chunkIndex = 0;
+
+          do {
+            if (remainingChunks[chunkIndex] > firstChunk)
+              lastChunkState = "bigger";
+            if (remainingChunks[chunkIndex] < firstChunk)
+              lastChunkState = "smaller";
+
+            chunkIndex++;
+          } while (
+            lastChunkState === "same" &&
+            chunkIndex < remainingChunks.length
+          );
+
+          // for, eg, 3638, we don't want to include 3636 - start at 3737
+          if (lastChunkState === "bigger") return firstChunk + 1;
+          else return firstChunk;
+        } else {
+          // return 10, 1000, 100000 etc
+          return parseInt(
+            `1${Array(factor - 1)
+              .fill("0")
+              .join("")}`,
+          );
+        }
+      })();
+
+      const endFirstChunk = (() => {
+        if (possibleLength === endLength) {
+          const [firstChunk, ...remainingChunks] = splitStringIntoChunks(
+            stringEnd,
+            factor,
+          ).map(Number);
+
+          let lastChunkState: "same" | "bigger" | "smaller" = "same";
+          let chunkIndex = 0;
+
+          do {
+            if (remainingChunks[chunkIndex] > firstChunk)
+              lastChunkState = "bigger";
+            if (remainingChunks[chunkIndex] < firstChunk)
+              lastChunkState = "smaller";
+
+            chunkIndex++;
+          } while (
+            lastChunkState === "same" &&
+            chunkIndex < remainingChunks.length
+          );
+
+          if (lastChunkState === "smaller") return firstChunk - 1;
+          else return firstChunk;
+        } else {
+          return parseInt(`${Array(factor).fill("9").join("")}`);
+        }
+      })();
+
+      if (startFirstChunk > endFirstChunk) {
+        console.log(`No possible invalid ids for chunk length ${factor}`);
+        continue;
       }
-    })();
 
-    const endFirstHalf = (() => {
-      if (possibleLength === endLength) {
-        const [firstHalf, secondHalf] = splitStringAtIndex(
-          stringEnd,
-          possibleLength / 2,
-        ).map(Number);
+      console.log(
+        `${startFirstChunk.toString().repeat(possibleLength / factor)} -> ${endFirstChunk.toString().repeat(possibleLength / factor)} (chunk length ${factor}): ${endFirstChunk - startFirstChunk + 1} invalid (${rangeInclusive(startFirstChunk, endFirstChunk).map((v) => `${v.toString().repeat(possibleLength / factor)}`)})`,
+      );
 
-        // for, eg, 3836, we don't want to include 3838 - end at 3737
-        if (firstHalf > secondHalf) return firstHalf - 1;
-        else return firstHalf;
-      } else {
-        // return 99, 9999, 999999 etc
-        return parseInt(
-          Array(possibleLength / 2)
-            .fill("9")
-            .join(""),
+      for (const firstHalf of rangeInclusive(startFirstChunk, endFirstChunk)) {
+        const number = parseInt(
+          firstHalf.toString().repeat(possibleLength / factor),
         );
+
+        if (number < start) throw new Error(`start ${number} ${start}`);
+        if (number > end) throw new Error(`end ${number} ${end}`);
+
+        invalidIds.add(number);
       }
-    })();
-
-    console.log(
-      `${startFirstHalf}${startFirstHalf} -> ${endFirstHalf}${endFirstHalf}: ${endFirstHalf - startFirstHalf + 1} invalid (${rangeInclusive(startFirstHalf, endFirstHalf).map((v) => `${v}${v}`)})`,
-    );
-
-    for (const firstHalf of rangeInclusive(startFirstHalf, endFirstHalf)) {
-      invalidIds += parseInt(`${firstHalf}${firstHalf}`);
     }
   }
 
-  return invalidIds;
+  console.log("========");
 }
 
 // =========
@@ -87,14 +140,12 @@ const input = String(await readFile("./input.txt"))
   .split(",")
   .map((v) => v.split("-").map(Number)) as Range[];
 
-console.log(input);
-
-let totalInvalid = 0;
-
 for (const range of input) {
-  console.log(`== Range:`, range);
-  totalInvalid += countInvalidIds(range);
-  console.log("==========");
+  countInvalidIds(range);
 }
 
-console.log(`Total invalid IDs: ${totalInvalid}`);
+const totalInvalid = [...invalidIds].reduce((acc, cur) => acc + cur, 0);
+
+console.log(
+  `Total invalid IDs: ${totalInvalid}. Took ${performance.now() - start} ms.`,
+);
